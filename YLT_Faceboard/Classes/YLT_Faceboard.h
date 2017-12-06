2d5d6d7a3bf899114cb125a2a98a5c37d0dddf38
QTEC <xiangph@qtec.cn>
2017-12-06 17:46:41 +0800

    release 0.0.2

--- YLT_Faceboard/Classes/YLT_Faceboard.h
+++ YLT_Faceboard/Classes/YLT_Faceboard.h
@@ -8,23 +8,8 @@
 #ifndef YLT_Faceboard_h
 #define YLT_Faceboard_h
 
-/**
- * 表情类型
- */
-typedef NS_ENUM(NSInteger, YLT_EmojiType) {
-    YLT_EmojiTypeEmoji,       // 基本emoji
-    YLT_EmojiTypeWealth,      // 财神表情
-    YLT_EmojiTypeFavorite,    // 我喜欢的
-    YLT_EmojiTypeGif,         // 远程下载的表情
-};
+#import "YLT_FaceboardView.h"
+#import "YLT_FaceboardConfig.h"
 
-/**
- * 表情包状态
- */
-typedef NS_ENUM(NSInteger, YLT_EmojiGroupStatus) {
-    YLT_EmojiGroupStatusUnDownload,
-    YLT_EmojiGroupStatusDownloaded,
-    YLT_EmojiGroupStatusDownloading,
-};
 
 #endif /* YLT_Faceboard_h */
